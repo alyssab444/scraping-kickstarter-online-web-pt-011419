1833408b0284ceee531f5edd145bdc55e5ddf17f
Alyssa Brown <alyssa.brown@live.com>
2019-03-01 02:42:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,8 @@
 require 'pry'
 require 'nokogiri'
 def create_project_hash
+  projects= {}
+  kickstarter.css("li.project.grid_4").each do |project| projects [project] = {}
   html=File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
